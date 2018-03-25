87e1890186c3f96dfcc3a7b7d0b8512142c83067
Mazaeval <mazaev.al.ni@gmail.com>
2018-03-25 20:35:29 +0300

    dynamic array

---
+++ Mazaev_Alexander_Task08/DynamicArray/DynamicArray.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicArrayClass
+{
+    public static class DynamicArrayDemo
+    {
+        public static void DemoMain()
+        {
+            DynamicArray<int> aar1 = new DynamicArray<int>();
+        }
+    }
+
+    public class DynamicArray<T> where T : new()
+    {
+        private T[] items;
+        private int size;
+        private int capacity;
+
+        public DynamicArray()
+        {
+            items = new T[8];
+            size = 0;
+        }
+
+        public DynamicArray(int n)
+        {
+            items = new T[n];
+            size = 0;
+        }
+
+        public DynamicArray(T[] a)
+        {
+            items = a;
+            size = a.Length;
+        }
+
+        public void Add(T elem)
+        {
+
+        }
+
+        public void AddRange(T[] a)
+        {
+
+        }
+
+        public bool Remove(T elem)
+        {
+            return true;
+        }
+
+        public void Insert(T[] a, int position)
+        {
+
+        }
+
+        public int Length()
+        {
+            return 1;
+        }
+
+        public int Capacity()
+        {
+            return 1;
+        }
+    }
+}
